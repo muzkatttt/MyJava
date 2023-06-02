3684b58bcc9cb0f2e34499bd43e899bc981fe969
Katerina <emrodionova@bk.ru>
2023-06-02 19:23:23 +0300

    added hw to sem2 OOP

---
+++ hwSem2OOP/src/CalcAbstract.java
@@ -0,0 +1,20 @@
+public abstract class CalcAbstract {
+    protected Double result;
+
+    protected Double a;
+    protected Double b;
+
+    protected Double addition(Double a, Double b) {
+        return result;
+    }
+    protected Double subtraction(Double a, Double b) {
+        return result;
+    }
+    protected Double multiplication(Double a, Double b) {
+        return result;
+    }
+    protected Double division(Double a, Double b) {
+        return result;
+    }
+
+}
