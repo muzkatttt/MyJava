9453fc9cc790cbb418d33d8abb9404abc9a36bdf
Katerina <emrodionova@bk.ru>
2023-06-05 21:43:30 +0300

    added packages with task to lesson1 OOP

---
+++ OOPLesson1/src/Task2/Point2D.java
@@ -0,0 +1,5 @@
+package Task2;
+public class Point2D  {
+    int x, y;
+    
+}
---
+++ OOPLesson1/src/Task5/Program.java
@@ -0,0 +1,15 @@
+package Task5;
+
+public class Program {
+    public static void main(String[] args) {
+        Magician hero1 = new Magician();
+        System.out.println(hero1.getInfo());
+        
+        Priest hero2 = new Priest();
+        System.out.println(hero2.getInfo());
+
+        Priest hero3 = new Priest();
+        System.out.println(hero3.getInfo());
+
+    }
+}
