96f42bd561e0d92b0dcfb34fe34ea62af061aa20
Katerina <emrodionova@bk.ru>
2023-06-18 23:04:02 +0300

    finished task conditions to homework to sem 6 OOP

--- OOPhwSem6/src/student/StudentComparator.java
+++ OOPhwSem6/src/student/StudentComparator.java
@@ -5,16 +5,20 @@ import java.util.Comparator;
 public class StudentComparator implements Comparator<Student> {
     @Override
     public int compare(Student o1, Student o2) {
-        int resultOfComparing = o1.getSecondName().compareTo(o2.getSecondName());
+        int resultOfComparing = o1.getUserId().compareTo(o2.getUserId());
         if (resultOfComparing == 0) {
-            resultOfComparing = o1.getFirstName().compareTo(o2.getFirstName());
+            resultOfComparing = o1.getSecondName().compareTo(o2.getSecondName());
             if (resultOfComparing == 0) {
-                return o1.getPatronymic().compareTo(o2.getPatronymic());
+                resultOfComparing = o1.getFirstName().compareTo(o2.getFirstName());
+                if (resultOfComparing == 0) {
+                    return o1.getPatronymic().compareTo(o2.getPatronymic());
+                }
+                return resultOfComparing;
             } else {
                 return resultOfComparing;
             }
-        } else {
-            return resultOfComparing;
         }
+        return resultOfComparing;
     }
 }
+
