bf8118908901364f6e8db53068b41d7c99dba6e4
Katerina <emrodionova@bk.ru>
2023-10-08 20:06:29 +0300

    unit-tests finished hw to sem5+

--- UnitTests/Test/seminars/five/homework/SaucedemoTest.java
+++ UnitTests/Test/seminars/five/homework/SaucedemoTest.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
@@ -28,21 +26,23 @@ public class SaucedemoTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
-        WebElement searchField = driver.findElement(By.id("user-name"));
-        searchField.sendKeys("standard_user");
-        searchField.submit();
-        wait(ExpectedConditions.visibilityOfElementLocated(By.id("user-name")));
-        WebElement searchField2 = driver.findElement(By.id("password"));
-        searchField2.sendKeys("secret_sauce");
-        searchField2.submit();
-        wait(ExpectedConditions.visibilityOfElementLocated(By.id("password")));
-        WebElement searchLoginButton = driver.findElement(By.id("login-button"));
+        WebElement searchFieldUserName = driver.findElement(By.id("user-name"));
+        searchFieldUserName.sendKeys("standard_user");
+        searchFieldUserName.submit();
+
+        WebElement searchFieldPassword = driver.findElement(By.id("password"));
+        searchFieldPassword.sendKeys("secret_sauce");
+        searchFieldPassword.submit();
+
+        //WebElement searchLoginButton = driver.findElement(By.id("login-button"));
+        WebElement searchLoginButton = driver.findElement(By.cssSelector("Login"));
+        searchLoginButton.submit();
         searchLoginButton.click();
 
-        List<WebElement> productsLabel = driver.findElements(By.cssSelector("div"));
+        List<WebElement> productsLabels = driver.findElements(By.className("title"));
         boolean a = false;
-        for (WebElement webElement : productsLabel) {
-            if (webElement.getText().contains("Products")) {
+        for (WebElement productsLabel : productsLabels) {
+            if (productsLabel.getText().contains("Products")) {
                 a = true;
                 break;
             }
@@ -51,7 +51,5 @@ public class SaucedemoTest {
         driver.quit();
     }
 
-    private void wait(ExpectedCondition<WebElement> webElementExpectedCondition) {
-    }
 }
 
