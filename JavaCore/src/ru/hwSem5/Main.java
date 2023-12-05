becaeb28a60f950b06847928a9cc938498818c5d
Katerina <emrodionova@bk.ru>
2023-12-05 21:15:32 +0300

    in progress hw to sem5 Java Core

--- JavaCore/src/ru/hwSem5/Main.java
+++ JavaCore/src/ru/hwSem5/Main.java
@@ -1,135 +1,71 @@
 package ru.hwSem5;
 
+
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 public class Main {
     /**
-     1.  Создать 2 текстовых файла, примерно по 50-100 символов в каждом(особого значения не имеет);
-     2.  Написать метод, «склеивающий» эти файлы, то есть вначале идет текст из первого файла,
-     потом текст из второго.
-     3.* Написать метод, который проверяет, присутствует ли указанное пользователем слово
-     в файле (работаем только с латиницей).
-     4.* Написать метод, проверяющий, есть ли указанное слово в папке
+     * TODO: Доработать метод print, необходимо распечатывать директории и файлы
      */
 
-    private static final Random random = new Random();
-    private static final int CHAR_BOUND_L = 65; // Номер начального символа
-    private static final int CHAR_BOUND_H = 90; // Номер конечного символа
-    private static final String TO_SEARCH = "GeekBrains";
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws IOException {
-
-        System.out.println(generateSymbols(15));
-        writeFileContents("sample01.txt", 30, TO_SEARCH);
-        writeFileContents("sample02.txt", 50, TO_SEARCH);
-        concatenate("sample01.txt", "sample02.txt", "sample_out.txt");
-
-        System.out.println(searchInFile("sample01.txt", TO_SEARCH));
-        System.out.println(searchInFile("sample02.txt", TO_SEARCH));
-
-        String[] fileName = new String[10];
-        for (int i = 0; i < fileName.length; i++){
-            fileName[i] = "file_" + i + ".txt";
-            writeFileContents(fileName[i], 15, TO_SEARCH);
-            System.out.printf("Файл %s создан.\n", fileName[i]);
-        }
-
-        List<String> list = searchMatch(new File("."), TO_SEARCH);
-        for (String s : list){
-            System.out.printf("Файл %s содержит искомое слово '%s'\n", s, TO_SEARCH);
-        }
-
+        // вызов функции print()
+        print(new File("."), " ", true);
     }
-
-    /**
-     * Метод генерации случайной последовательности символов
-     * @param count кол-во символов
-     * @return
-     */
-    static String generateSymbols(int count){
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < count; i++){
-            stringBuilder.append((char) random.nextInt(CHAR_BOUND_L, CHAR_BOUND_H  + 1));
+    static void print(File file, String indent, boolean isLast) {
+        System.out.print(indent);
+        if (isLast) {
+            System.out.print("└─");
+            indent += "  ";
+        } else {
+            System.out.print("├─");
+            indent += "│ ";
         }
-        return stringBuilder.toString();
-    }
+        //System.out.println(file.getName());
+        System.out.print(" ");
 
-    static void writeFileContents(String fileName, int count) throws IOException {
-        try(FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
-            fileOutputStream.write(generateSymbols(count).getBytes(StandardCharsets.UTF_8));
+        // добавлена проверка условия для определения, является ли файл каталогом
+        // если каталог существует, то распечатывается название каталога, если условие ложно,
+        // то распечатывается название файла с расширением
+        if (file.isDirectory()) {
+            System.out.println(file.getName() + " ");
+        } else {
+            System.out.println("file: " + file.getName());
+            return;
         }
-    }
 
-    static void writeFileContents(String fileName, int count, String toSearch) throws IOException {
-        try(FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
-            fileOutputStream.write(generateSymbols(count).getBytes(StandardCharsets.UTF_8));
-            if (random.nextInt(2) == 0){ // 50%
-                fileOutputStream.write(toSearch.getBytes(StandardCharsets.UTF_8));
-                fileOutputStream.write(generateSymbols(count).getBytes(StandardCharsets.UTF_8));
-            }
-        }
-    }
-
-    static void concatenate(String fileIn1, String fileIn2, String fileOut) throws IOException{
-        // На запись
-        try (FileOutputStream fileOutputStream = new FileOutputStream(fileOut)){
-            int c;
-            // На чтение
-            try (FileInputStream fileInputStream = new FileInputStream(fileIn1)){
-                while ((c = fileInputStream.read()) != -1){
-                    fileOutputStream.write(c);
-                }
-            }
-            // На чтение
-            try (FileInputStream fileInputStream = new FileInputStream(fileIn2)){
-                while ((c = fileInputStream.read()) != -1){
-                    fileOutputStream.write(c);
-                }
-            }
-        }
-    }
-
-    static boolean searchInFile(String fileName, String searchWord) throws IOException{
-        int c;
-        byte[] searchData = searchWord.getBytes();
-        int counter = 0;
-        try (FileInputStream fileInputStream = new FileInputStream(fileName)){
-            while ((c = fileInputStream.read()) != -1){
-                if (c == searchData[counter]){
-                    counter++;
-                }
-                else {
-                    counter = 0;
-                    if (c == searchData[counter])
-                        counter++;
-                }
-                if (counter == searchData.length){
-                    return true;
-                }
-            }
-            return false;
-        }
-    }
-
-    static List<String> searchMatch(File dir, String search) throws IOException {
-        List<String> list = new ArrayList<>();
-        File[] files = dir.listFiles();
+        File[] files = file.listFiles();
         if (files == null)
-            return list;
-        for (int i = 0; i < files.length; i++){
-            if (files[i].isFile()){
-                if (searchInFile(files[i].getCanonicalPath(), search))
-                    list.add(files[i].getCanonicalPath());
+            return;
+
+        int subDirTotal = 0;
+        int fileCount = 0;
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory()) {
+                subDirTotal++;
+            } else {
+                fileCount++;
             }
         }
-        return list;
-    }
 
+        int subDirCounter = 0;
+        // добавила еще один счетчик для проверки количества файлов и их добавления в дерево при печати
+        int fileCount1 = 0;
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory()) {
+                // вызов функции print() для печати только наименовуния папок
+                //print(files[i], indent, subDirTotal == ++subDirCounter);
+                // вызов метода print() для печати папок и файлов
+
+                print(files[i], indent, subDirCounter == subDirTotal - 1 && fileCount1 == fileCount);
+                subDirCounter++;
+            } else {
+                print(files[i], indent, fileCount1 == fileCount - 1 && subDirCounter == subDirTotal);
+                fileCount1++;
+            }
+        }
+    }
 }
+
+
