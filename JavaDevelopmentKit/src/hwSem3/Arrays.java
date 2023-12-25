4501238e943de7ab60327095691f66a4039b5f57
Katerina <emrodionova@bk.ru>
2023-12-25 22:33:48 +0300

    finished hw to sem3 Java Development Kit++

--- JavaDevelopmentKit/src/hwSem3/Arrays.java
+++ JavaDevelopmentKit/src/hwSem3/Arrays.java
@@ -31,11 +31,9 @@ public class Arrays {
      */
     public static <T> boolean compareArrays(T[] array1, T[] array2) {
         if (array1.length == array2.length) {
-
             for (int i = 0; i < array1.length; i++) {
                 for (int j = 0; j < array2.length; j++) {
-                    if (array1[i].getClass().equals(array2[j].getClass())) {
-                        // && (array1[i].equals(array2[i]))
+                    if (array1[i].getClass().equals(array2[j].getClass()) && (array1[i].equals(array2[i]))) {
                         return true;
                     }
                 }
@@ -45,5 +43,4 @@ public class Arrays {
         }
         return false;
     }
-
 }
--- JavaDevelopmentKit/src/hwSem3/Calculate.java
+++ JavaDevelopmentKit/src/hwSem3/Calculate.java
@@ -10,51 +10,52 @@ public class Calculate {
         System.out.println(sum(2, 2.5));
         System.out.println(multiply(2f, 3.52));
         System.out.println(divide(9.00, 3f));
+        //System.out.println(divide(6, 0));
         System.out.println(subtract(10, 5L));
     }
 
     /**
-     *
-     * @param t слагаемое
-     * @param v слагаемое
-     * @return сумма чисел t + v
+     * @param t   слагаемое
+     * @param v   слагаемое
      * @param <T> первое число extends Number
      * @param <V> второе число extends Number
+     * @return сумма чисел t + v
      */
     public static <T extends Number, V extends Number> double sum(T t, V v) {
         return Math.round(t.doubleValue() + v.doubleValue());
     }
 
     /**
-     *
-     * @param t первый множитель
-     * @param v второй множитель
-     * @return произведение чисел t * v
+     * @param t   первый множитель
+     * @param v   второй множитель
      * @param <T> первое число extends Number
      * @param <V> второе число extends Number
+     * @return произведение чисел t * v
      */
     public static <T extends Number, V extends Number> double multiply(T t, V v) {
         return Math.round(t.doubleValue() * v.doubleValue());
     }
 
     /**
-     * @param t делимое
-     * @param v делитель
-     * @return частное чисел t / v
+     * @param t   делимое
+     * @param v   делитель
      * @param <T> первое число extends Number
      * @param <V> второе число extends Number
+     * @return частное чисел t / v
      */
     public static <T extends Number, V extends Number> double divide(T t, V v) {
+        if (v.doubleValue() == 0) {
+            throw new ArithmeticException("Деление на ноль недопустимо");
+        }
         return Math.round(t.doubleValue() / v.doubleValue());
     }
 
     /**
-     *
-     * @param t уменьшаемое
-     * @param v вычитаемое
-     * @return разность чисел t - v
+     * @param t   уменьшаемое
+     * @param v   вычитаемое
      * @param <T> первое число extends Number
      * @param <V> второе число extends Number
+     * @return разность чисел t - v
      */
     public static <T extends Number, V extends Number> double subtract(T t, V v) {
         return Math.round(t.doubleValue() - v.doubleValue());
