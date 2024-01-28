e94fbbaca73eb163496f9963e55fc1f2deb7d9ee
Katerina <emrodionova@bk.ru>
2024-01-28 14:50:13 +0300

    added task to sem1 Java Junior

---
+++ JavaJunior/src/sem1/task2/Thing.java
@@ -0,0 +1,15 @@
+package sem1.task2;
+
+/**
+ * Вещь
+ */
+public interface Thing {
+
+    /**
+     * Получить наименование вещи
+     * @return наименование вещи
+     */
+    String getName();
+
+}
+
