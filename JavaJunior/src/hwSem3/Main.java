14f25ccea212026534f504d9ba4abd323583a20e
Katerina <emrodionova@bk.ru>
2024-02-07 22:52:20 +0300

    in progress hw to sem3 Java Junior

--- JavaJunior/src/hwSem3/Main.java
+++ JavaJunior/src/hwSem3/Main.java
@@ -1,7 +1,25 @@
 package hwSem3;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class Main {
     public static void main(String[] args) {
+        Student student = Student.create("James Gosling", 38, 99.99);
+        System.out.println(student);
 
+        // сериализовать student
+        try (FileOutputStream fileOut = new FileOutputStream("student.txt");
+             ObjectOutputStream out = new ObjectOutputStream(fileOut))
+        {
+            out.writeObject(student);
+            System.out.printf("Объект %s сериализован.\n", student.getClass().getName());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ JavaJunior/src/hwSem3/Student.java
@@ -0,0 +1,44 @@
+package hwSem3;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+
+    private String name;
+
+    private int age;
+
+    transient double GPA; // средний балл студента
+
+    public Student(String name, int age, double GPA) {
+        this.name = name;
+        this.age = age;
+        this.GPA = GPA;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    public static Student create(String name, int age, double GPA) {
+        return new Student(name, age, GPA);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Student{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", age=").append(age);
+        sb.append(", GPA=").append(GPA);
+        sb.append('}');
+        return sb.toString();
+    }
+}
