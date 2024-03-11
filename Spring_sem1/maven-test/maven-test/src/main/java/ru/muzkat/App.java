7af36bb213bf1eedb24b96b4fde9a971c7b93d9c
Katerina <emrodionova@bk.ru>
2024-03-11 21:52:44 +0300

    added ferst commit maven-test (sem1 Spring)

---
+++ Spring_sem1/maven-test/maven-test/src/main/java/ru/muzkat/App.java
@@ -0,0 +1,13 @@
+package ru.muzkat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
